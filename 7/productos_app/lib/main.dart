5cdb7a0bec32272203b5e9a25c38d0872325caa4
Beatriz <beatriz.delpinal@optima.com.sv>
2022-07-05 14:08:05 -0600

    end of section 12

--- 7/productos_app/lib/main.dart
+++ 7/productos_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/screens/screens.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,16 +8,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      title: 'Productos App',
+      initialRoute: LoginScreen.routerName,
+      routes: {
+        HomeScreen.routerName   : ( _ ) => const HomeScreen(),
+        LoginScreen.routerName  : ( _ ) => const LoginScreen(),
+      },
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: Colors.grey[300]
       ),
     );
   }
---
+++ 7/productos_app/lib/providers/login_form_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginFormProvider extends ChangeNotifier {
+GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  String email    = '';
+  String password = '';
+
+  bool _isloading = false;
+  bool  get isLoading => _isloading;
+  set isLoading(bool value){
+    _isloading = value;
+    notifyListeners();
+  }
+
+  bool isValidForm() {
+    print(formKey.currentState?.validate());
+    print('$email - $password');
+    return formKey.currentState?.validate() ?? false;
+  }
+}
